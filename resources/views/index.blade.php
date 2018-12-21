253622665bad8ea5644919f6dd724bae060fec8b
Laymont Arratia <laymont@gmail.com>
2018-12-21 10:12:01 -0400

    New Ini

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,27 +3,7 @@
 @section('content')
 <div class="container-fluid">
   <div class="row">
-    <nav class="navbar navbar-light bg-faded">
-      <a class="navbar-brand" href="#">{4:Navbar}</a>
-      <ul class="nav navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-      </ul>
-      <form class="form-inline navbar-form pull-right">
-        <input class="form-control" type="text" placeholder="Search">
-        <button class="btn btn-success-outline" type="submit">Search</button>
-      </form>
-    </nav>
+
   </div>
 </div>
 @endsection
